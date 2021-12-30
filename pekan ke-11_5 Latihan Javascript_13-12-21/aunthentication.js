7d721c3519f2b361952e0552d5170efbb16cb238
Muzaki Abdullah Irsyad <92155347+Muza900ki@users.noreply.github.com>
2021-12-31 05:28:47 +0700

    Praktikum 8 Java Script

---
+++ pekan ke-11_5 Latihan Javascript_13-12-21/aunthentication.js
@@ -0,0 +1,15 @@
+
+            let username=document.getElementById("username")
+            let password=document.getElementById("password")
+
+            function login ()
+            {
+                    if(username.value=="ahmad2017" && password.value=="integrity")
+                {
+                    
+                    alert("succses")
+                   window.location="succeslogin.html"
+                }   else {
+                    alert("error")
+                         }
+            }
